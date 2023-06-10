eac3dce38505f5b9d2bbc3c25e4dee3c0ce0fd3a
mananchaya04 <118960144+mananchaya04@users.noreply.github.com>
2023-06-10 11:59:21 +0700

    Club

--- main/src/Assess.java
+++ main/src/Assess.java
@@ -2,15 +2,7 @@ import java.util.Scanner;
 
 public class Assess {
     private String[] teachers = {"Thai", "Eng", "Math", "Science"};
-    double[][] scores = new double[4][3];
-
-    public String[] getTeachers() {
-        return teachers;
-    }
-
-    public void setTeachers(String[] teachers) {
-        this.teachers = teachers;
-    }
+    private double[][] scores = new double[4][3];
 
     public void inputScores() {
         Scanner scanner = new Scanner(System.in);
@@ -18,32 +10,53 @@ public class Assess {
         for (int i = 0; i < teachers.length; i++) {
             System.out.println("Enter scores for " + teachers[i] + " teacher");
 
-            System.out.print("1.teaching materials and equipment : ");
+            System.out.print("1. Teaching materials and equipment: ");
             scores[i][0] = validateScoreInput(scanner);
 
-            System.out.print("2. suitability to level of knowledge and learner's ability : ");
+            System.out.print("2. Suitability to level of knowledge and learner's ability: ");
             scores[i][1] = validateScoreInput(scanner);
 
-            System.out.print("3. Coverage and alignment with purpose : ");
+            System.out.print("3. Coverage and alignment with purpose: ");
             scores[i][2] = validateScoreInput(scanner);
 
             System.out.println("-----------------------------------------------------------------");
         }
+        scanner.close(); // Close the scanner after usage
     }
 
     private double validateScoreInput(Scanner scanner) {
         while (true) {
-            double score = scanner.nextDouble();
-            if (score >= 1 && score <= 5) {
-                return score;
+            if (scanner.hasNextDouble()) {
+                double score = scanner.nextDouble();
+                if (score >= 1 && score <= 5) {
+                    return score;
+                } else {
+                    System.out.print("!!! Wrong number, please enter again: ");
+                }
             } else {
-                System.out.print(" !!! Wrong number, please enter again : ");
+                String input = scanner.next();
+                System.out.print("!!! Invalid input, please enter a number: ");
             }
         }
+    }
+
+    public double[][] getScores() {
+        return scores;
     }
 
     public static void main(String[] args) {
         Assess assess = new Assess();
         assess.inputScores();
+
+        double[][] scores = assess.getScores();
+
+        // Print the scores for verification
+        for (int i = 0; i < scores.length; i++) {
+            System.out.println("Scores for " + assess.teachers[i] + " teacher:");
+            for (int j = 0; j < scores[i].length; j++) {
+                System.out.println("Criteria " + (j + 1) + ": " + scores[i][j]);
+            }
+            System.out.println("---------------------------------------");
+        }
     }
 }
